0f4cb711265816d7597d5c9c3da56b9a7bd36411
Sveinung Dalatun <sdalatun@gmail.com>
2013-04-09 22:14:19 +0200

    Deploy the app to node1

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#scp -P 2222 build/libs/provisioning.jar vagrant@localhost:/home/vagrant/
-vagrant ssh node1 -c "/vagrant/deploy/install.sh"
+scp -P 2200 build/libs/app.jar vagrant@localhost:/home/vagrant/
