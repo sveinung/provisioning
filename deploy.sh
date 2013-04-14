613e26865a5ba6e47efa647c8eda039c2cf3e8d6
Sveinung Dalatun <sdalatun@gmail.com>
2013-04-14 16:30:58 +0200

    Create a master connection used for deployment to avoid having to enter the password multiple times

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,13 @@ USER=vagrant
 HOST=localhost
 PORT=2200
 
-scp -P $PORT build/libs/app.jar $USER@$HOST:/home/vagrant/
-scp -P $PORT -r deploy/ $USER@$HOST:/home/vagrant
-ssh -p $PORT -t $USER@$HOST "deploy/install.sh"
+SSH_SOCKET=~/.ssh/$USER@$HOST
+
+ssh -M -f -N -p $PORT -o ControlPath=$SSH_SOCKET $USER@$HOST
+
+scp -o ControlPath=$SSH_SOCKET build/libs/app.jar $USER@$HOST:/home/vagrant/
+scp -o ControlPath=$SSH_SOCKET -r deploy/ $USER@$HOST:/home/vagrant
+ssh -o ControlPath=$SSH_SOCKET -t $USER@$HOST "deploy/install.sh"
+
+ssh -S $SSH_SOCKET -O exit $USER@$HOST
 
