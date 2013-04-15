f0508cdac88a2e543f11e71119738730c6c11708
Sveinung Dalatun <sdalatun@gmail.com>
2013-04-15 22:07:42 +0200

    Run host specific provisioning functions

--- javascript/infrastructure/setup.js
+++ javascript/infrastructure/setup.js
@@ -1,4 +1,23 @@
 #!/usr/bin/shjs
 
+require('shelljs/global');
+var _  = require('underscore');
+_.str = require('underscore.string');
+_.mixin(_.str.exports());
+
 console.log("Provisioning with JavaScript");
 
+nodes({
+  'master.lan': function() {
+    console.log("Master");
+  },
+  'node1.lan': function() {
+    console.log("Node 1");
+  }
+});
+
+function nodes(hosts) {
+  var hostname = _.trim(exec('hostname').output);
+  var currentNode = hosts[hostname];
+  currentNode();
+}
