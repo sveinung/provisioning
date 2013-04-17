806c961f065d38dba453ecdd28b5a6dff1132e1c
Sveinung Dalatun <sdalatun@gmail.com>
2013-04-17 21:26:37 +0200

    Copy the nginx.conf to its place

--- javascript/infrastructure/nginx/nginx.js
+++ javascript/infrastructure/nginx/nginx.js
@@ -1,9 +1,15 @@
 var pkg = require('../provisioner/pkg');
+var file = require('../provisioner/file');
 
 var nginx = function() {
   pkg({
     name: 'nginx',
     version: '1.1.19*'
+  });
+
+  file({
+    name: '/etc/nginx/nginx.conf',
+    source: __dirname + '/nginx.conf'
   });
 };
 
